756ca108518c8a63018fe82839e7e7ed403738ed
N LirajKhanna <lirajkhanna2002@gmail.com>
2021-11-15 09:15:59 +0530

    Initial Commit

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+//* IMPORTS
+const express = require("express");
+require("dotenv").config({path: "./backend/config/config.env"});
+
+//* CONSTANTS
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+//* ROUTES
+app.get("/", (req, res) => {
+  res.send("Welcome to Social Media App By N LirajKhanna");
+});
+
+//* GENERAL CONFIG
+app.listen(PORT, () => {
+  console.log(`Server up and running on ${PORT}`);
+});
